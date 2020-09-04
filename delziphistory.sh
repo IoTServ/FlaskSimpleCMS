271a246c5b94b9a33645a18723821c61d3528d40
iotserv <yu@iotserv.com>
2020-09-04 12:12:14 +0800

    script

--- delziphistory.sh
+++ delziphistory.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *.zip' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch app/main/views.py' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch app/user/views.py' --prune-empty --tag-name-filter cat -- --all
 git push origin master --force
